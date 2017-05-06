b36da6a2a38693efbb0e8f321f5e216d6758cf2b
Nasim Mahmud <gen.lee.shin@gmail.com>
2017-05-06 03:01:47 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP Plugin Boilerplate
+ * Plugin URI: http://www.anyhours.co.uk
+ * Author: Nasim Mahmud
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+new App\App();
